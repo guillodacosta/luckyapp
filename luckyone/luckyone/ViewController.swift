2e5c61555abd12c3f5d56bcdbdad1090f8e169fc
guillermo <guillodiacosta@gmail.com>
2019-12-18 16:46:18 -0500

    Project creation

---
+++ luckyone/luckyone/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  luckyone
+//
+//  Created by guillermo on 12/18/19.
+//  Copyright © 2019 guillermo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
